658a61368d0ce34b7a22c342bd9653fdcbd5e9eb
Cedric <3326546+cdurau@users.noreply.github.com>
2021-03-29 00:45:26 +0200

    Added Materialize CSS

--- app/Views/UserView.php
+++ app/Views/UserView.php
@@ -1,5 +1,14 @@
-<div class="card">
-    <img src="<?= $user['picture']; ?>" alt="">
-    <h3 class="name"><?= $user["firstName"] . " " . $user["lastName"]; ?></h3>
-    <p class="email"><?= $user["email"]; ?></p>
+<div class="col s12 m4 xl3">
+    <div class="card">
+        <div class="card-image">
+            <img src="<?= $user['picture']; ?>" alt="">
+            <span class="card-title"><?= $user["firstName"] . " " . $user["lastName"]; ?></span>
+        </div><!-- .card-image -->
+        <div class="card-content">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aliquam dolor laboriosam eveniet, quos fugiat quasi ipsam repellendus eum corrupti!</p>
+        </div><!-- .card-content -->
+        <div class="card-action">
+            <a class="btn-floating amber z-depth-2" href="mailto:<?= $user['email']; ?>"><i class="material-icons">mail_outline</i></a>
+        </div><!-- .card-action -->
+    </div><!-- .card -->
 </div>
--- public/index.php
+++ public/index.php
@@ -5,15 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>MVC Framework</title>
 </head>
 
 <body>
     <div class="container">
-        <?php require_once('../app/bootstrap.php'); ?>
+        <div class="row ">
+            <?php require_once('../app/bootstrap.php'); ?>
+        </div>
     </div>
 
+    <!-- MaterializeCSS JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 
 </html>
